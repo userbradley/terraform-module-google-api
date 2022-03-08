a8925579bfafdbe9e511e4a4c33ab9006354862b
Bradley <bradley@breadnet.co.uk>
2022-03-08 16:25:32 +0000

    resolved issue

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "google_project_service" "project" {
   for_each = toset(var.api)
   service = each.key
-disable_dependent_services = var.dds
   timeouts {
     create = "20m"
     read = "10m"
@@ -9,6 +8,6 @@ disable_dependent_services = var.dds
     delete = "20m"
   }
 
-  disable_dependent_services = false # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service#disable_dependent_services
+  disable_dependent_services = var.dds # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service#disable_dependent_services
   disable_on_destroy = true
 }
